725da2969f3fbd7fcb0f0e8ef8b05b15a2c0cf14
David Andres Valero Vanegas <dvdovni@gmail.com>
2019-02-23 00:14:17 -0500

    Se quitan impresiones innecesarias y se mejora la salida al colocarle textos personalizados

--- Contraseñas.py
+++ Contraseñas.py
@@ -1,13 +1,9 @@
 from typing import List
 
 letras: List[str]="a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
-print(letras)
 opcion=input("Ingrese 1 para cifrar y 2 para decifrar")
-print(opcion)
 mensaje=input("Ingrese el mensaje")
 clave=input("Ingrese la clave")
-print("El mensaje es ",mensaje)
-print("la clave es ",clave)
 criptograma=""
 auxClave=clave.__len__()
 cont=0
@@ -20,4 +16,7 @@ for i in mensaje:
         criptograma += letras[(letras.index(i)+26-(letras.index(clave[cont % auxClave]))) % 26]
         cont +=1
 
-print(criptograma)
+if opcion=="1":
+    print("El mensaje ",mensaje, "cifrado con la clave ",clave, "es ",criptograma)
+elif opcion=="2":
+    print("El criptograma ",mensaje, "descifrado con la clave ",clave, "significa ",criptograma)
